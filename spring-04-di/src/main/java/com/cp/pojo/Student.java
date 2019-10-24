a88707a5aa0d8a3b60a418fbbecd5e3e2c595b26
chenpeng <770047395@qq.com>
2019-10-24 11:21:50 +0800

    bean属性注入

---
+++ spring-04-di/src/main/java/com/cp/pojo/Student.java
@@ -0,0 +1,92 @@
+package com.cp.pojo;
+
+import java.util.*;
+
+public class Student {
+    private String name;
+    private Address address;
+    private String[] book;
+    private List<String> hobbys;
+    private Map<String,String> card;
+    private Set<String> games;
+    private Properties info;
+    private String wife;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String[] getBook() {
+        return book;
+    }
+
+    public void setBook(String[] book) {
+        this.book = book;
+    }
+
+    public List<String> getHobbys() {
+        return hobbys;
+    }
+
+    public void setHobbys(List<String> hobbys) {
+        this.hobbys = hobbys;
+    }
+
+    public Map<String, String> getCard() {
+        return card;
+    }
+
+    public void setCard(Map<String, String> card) {
+        this.card = card;
+    }
+
+    public Set<String> getGames() {
+        return games;
+    }
+
+    public void setGames(Set<String> games) {
+        this.games = games;
+    }
+
+    public Properties getInfo() {
+        return info;
+    }
+
+    public void setInfo(Properties info) {
+        this.info = info;
+    }
+
+    public String getWife() {
+        return wife;
+    }
+
+    public void setWife(String wife) {
+        this.wife = wife;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                ", book=" + Arrays.toString(book) +
+                ", hobbys=" + hobbys +
+                ", card=" + card +
+                ", games=" + games +
+                ", info=" + info +
+                ", wife='" + wife + '\'' +
+                '}';
+    }
+}
---
+++ spring-04-di/src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import com.cp.pojo.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student.toString());
+    }
+}
